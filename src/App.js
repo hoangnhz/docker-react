17f72439a46eb4b4a2c69bcdcf5cf48903fa5465
hoangnhz <hoangnh.devops@gmail.com>
2023-04-12 12:16:24 +0700

    fix deploy error_3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps
+          Learn React
         </a>
       </header>
     </div>
