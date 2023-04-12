3ed12efe678809561b97aceccb70ed5fc3b8337e
hoangnhz <hoangnh.devops@gmail.com>
2023-04-12 21:56:41 +0700

    changed app text_1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Master
+          DevOps Expert
         </p>
         <a
           className="App-link"
